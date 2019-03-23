24255a31e0ff3b036db9767afe213c52633d9156
wasabi <richardlikeswasabi@gmail.com>
2019-03-23 15:38:42 +1100

    Added stop words

--- sam.py
+++ sam.py
@@ -2,7 +2,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 
 
@@ -41,25 +43,53 @@ if __name__ == "__main__":
     path = "incidents.csv"
     train_data, train_label, test_data, test_label, unseen_data, unseen_label = read_csv(path)
     
-    text_clf = Pipeline([('vect', CountVectorizer()),
-                         ('tfidf', TfidfTransformer()),
-                         ('clf', MultinomialNB())])
+    MODEL = "NB"
+    if MODEL == "NB":
+        text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
+                             ('tfidf', TfidfTransformer()),
+                             ('clf', MultinomialNB())])
+        parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
+                      'tfidf__use_idf': (True, False),
+                      'clf__alpha': (1e-2, 1e-3)}
+    elif MODEL == "SVM":
+        text_clf = Pipeline([('vect', CountVectorizer()),
+                             ('tfidf', TfidfTransformer()),
+                             ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                                   alpha=1e-3,  
+                                                   random_state=42))
+                             ])
+        parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
+                      'tfidf__use_idf': (True, False),
+                      'clf__alpha': (1e-2, 1e-3)}
 
     text_clf = text_clf.fit(train_data, train_label)
-    #twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
     predicted = text_clf.predict(test_data)
+    print("non grid search", np.mean(predicted == test_label))
     
+
+    
+    
+    gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
+    gs_clf = gs_clf.fit(train_data, train_label)
+    predicted = gs_clf.predict(test_data)
+    print("grid search", np.mean(predicted == test_label))
+    #print(gs_clf.best_score_)
+    #print(gs_clf.best_params_)
+
+
     # Model validation
     print(np.mean(predicted == test_label))
-    for i in range(len(test_data)):
-        print(test_data[i], '|', predicted[i], '|', test_label[i])
+
+    """
+    #for i in range(len(test_data)):
+        #print(test_data[i], '|', predicted[i], '|', test_label[i])
 
     # Testing against unseen 
     unseen_predicted = text_clf.predict(unseen_data)
     #print(np.mean(predicted == test_label))
     no, yes = 0, 0
     for i in range(len(unseen_data)):
-        print(unseen_data[i], '|', unseen_predicted[i])
+        #print(unseen_data[i], '|', unseen_predicted[i])
         if unseen_predicted[i] == 'y':
             yes += 1
         else:
@@ -67,4 +97,4 @@ if __name__ == "__main__":
 
     print("Yes:", yes, "No:" , no)
 
-
+"""
